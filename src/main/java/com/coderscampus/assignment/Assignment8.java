be86b4b3dd3b9d0b93a79ebb404636b87032a4a0
Jamie Gable <146774360+jamiegable24@users.noreply.github.com>
2024-11-13 12:34:43 -0600

    Completed Assignment 8

--- src/main/java/com/coderscampus/assignment/Assignment8.java
+++ src/main/java/com/coderscampus/assignment/Assignment8.java
@@ -3,8 +3,14 @@ package com.coderscampus.assignment;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -26,6 +32,49 @@ public class Assignment8 {
         }
     }
 
+    private CompletableFuture<List<Integer>> getData() throws IOException {
+
+        ExecutorService ioBoundTask = Executors.newCachedThreadPool();
+
+        List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
+
+        for (int x = 0; x < 1000; x++) {
+            CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(this::getNumbers, ioBoundTask);
+            futures.add(future);
+        }
+
+        while (futures.stream().filter(CompletableFuture::isDone).count() < 1000) {
+        }
+
+        System.out.println("Fetching complete!");
+
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenApplyAsync(
+                a -> futures.stream().flatMap(future -> future.join().stream()).collect(Collectors.toList()),
+                ioBoundTask);
+    }
+
+
+    private void integerFrequency(List<Integer> integerList) {
+        Map<Integer, Long> frequencyMap = integerList.stream()
+                .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+        frequencyMap.forEach((key, value) -> System.out
+                .println(withLargeIntegers(key) + "=" + withLargeIntegers(Integer.parseInt(String.valueOf(value)))));
+    }
+
+
+    public static String withLargeIntegers(Integer n) {
+        DecimalFormat df = new DecimalFormat("###,###,###");
+        return df.format(n);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+        Assignment8 assignment8MainApp = new Assignment8();
+        List<Integer> resultingNumberList = assignment8MainApp.getData().get();
+        System.out.println("Total number of integers: " + withLargeIntegers(resultingNumberList.size()));
+        assignment8MainApp.integerFrequency(resultingNumberList);
+        System.exit(0);
+    }
     /**
      * This method will return the numbers that you'll need to process from the list
      * of Integers. However, it can only return 1000 records at a time. You will
